5d4028c5e23d101450256bb10c1acea96e58b301
dlq <59379788@qq.com>
2016-08-08 17:44:09 +0800

    合计

--- app/14statistics/controllers/statisticsdetail.js
+++ app/14statistics/controllers/statisticsdetail.js
@@ -17,6 +17,11 @@ module.exports = function($scope, day, sale_name, sale_code, orderstatisticsused
 			company_code : company_code
 		};
 
+		$scope.total = {
+            'used' : 0,
+            'total' : 0
+        };
+
 		if(operation_login_name !== undefined) {
 			para["operation_login_name"] = operation_login_name;
 		}
@@ -26,6 +31,11 @@ module.exports = function($scope, day, sale_name, sale_code, orderstatisticsused
 			if(res.errcode === 0)
 			{
 				$scope.objs = res.data;
+				for(var i = 0, j = res.data.length; i < j; i++)
+                {
+                    $scope.total.used += parseInt(res.data[i].used);
+                    $scope.total.total += parseInt(res.data[i].used * res.data[i].unit_price);
+                }
 			}
 			else
 			{
