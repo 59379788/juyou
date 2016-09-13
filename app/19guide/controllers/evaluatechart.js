a056f9f86a35aa080f331649c42d44f2351c7f94
dlq <59379788@qq.com>
2016-09-13 12:08:44 +0800

    aaaa

--- app/19guide/controllers/evaluatechart.js
+++ app/19guide/controllers/evaluatechart.js
@@ -3,5 +3,20 @@ module.exports = function($scope){
       $scope.labels = ["非常满意", "一般满意", "不满意"];
       $scope.data1 = [35, 10, 5];
       $scope.data2 = [40, 7, 3];
+
+
+
+      $scope.toggle = function () {
+          $scope.type = $scope.type === 'polarArea' ?
+            'pie' : 'polarArea';
+        };
     
+    // $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+    // $scope.data1 = [300, 500, 100, 40, 120];
+    // $scope.type = 'polarArea';
+
+    // $scope.toggle = function () {
+    //   $scope.type = $scope.type === 'polarArea' ?
+    //     'pie' : 'polarArea';
+    // };
 };
