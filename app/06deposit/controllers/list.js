0c7947de3254923296f4fb07484b82bac9f76e4a
ml <ma_0725@163.com>
2016-10-03 19:31:57 +0800

    重复字段不重复添加

--- app/06deposit/controllers/list.js
+++ app/06deposit/controllers/list.js
@@ -4,25 +4,44 @@ module.exports = function($scope, $state, list, ITEMS_PERPAGE, mechanism,
   	$scope.searchform = {};
     $scope.seller_name ;
 
+    //页面内商户搜索
     $scope.searchbyname = function(){
+
+        //不传值就所有结果都显示
         $scope.aggregate = new Array();
         if (($scope.seller_name == undefined) || ($scope.seller_name == "") ) {
-          console.log($scope.seller_name);
           $scope.aggregate = $scope.objs;
           return;
         }
+
+        //输入字符串的长度len
         var len = $scope.seller_name.length ;
-        console.log(len);
+
+        //遍历所有结果
         for (var i = $scope.objs.length - 1; i >= 0; i--) {
+
+          //结果字符串长度不小于要搜索的字符串长度
           if ($scope.objs[i].seller_name.length >= len) {
+
+            //遍历目标字符串的前len个到后len个
             for (var j = 0; $scope.objs[i].seller_name.length - j >= 0; j++) {
+
+              //所取字符串和输入的比，相同就加到要显示的结果集中
               if($scope.objs[i].seller_name.substring(j, j + len) == $scope.seller_name) {
                 $scope.aggregate.push($scope.objs[i]);
+
+                //加到结果集中则无需继续遍历此目标字符串
+                break;
               }
             }
           }
         }
-        console.log($scope.aggregate);
+
+        //遍历后没有符合条件的就全显示
+        if ($scope.aggregate.length == 0) {
+          $scope.aggregate = $scope.objs;
+          alert("没有找到您输入的商户，请重新输入！")
+        }
         // return;
     }
 
