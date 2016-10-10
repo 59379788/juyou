ca078a5a11592677a9cf69c50d0486446727734c
dlq <59379788@qq.com>
2016-10-10 12:13:53 +0800

    90套票

--- app/12sale/router.js
+++ app/12sale/router.js
@@ -215,8 +215,8 @@ var router = function($urlRouterProvider, $stateProvider){
             insertWinterTicketOrder : function(sellingservice){
                 return sellingservice.insertWinterTicketOrder();
             },
-            createOrder : function(sellingservice){
-                return sellingservice.createOrder();
+            createOrder : function(orderservice){
+                return orderservice.createOrder();
             }
         }
       })
