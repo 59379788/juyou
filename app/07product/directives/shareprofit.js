59e147d80b3d3718607924e51426245e88132b99
小裙子 <18842678305@163.com>
2017-03-16 13:49:40 +0800

    新的创建销售品

--- app/07product/directives/shareprofit.js
+++ app/07product/directives/shareprofit.js
@@ -10,21 +10,74 @@ module.exports = function($resource, $state, $http, $q){
 			'util' : '=',
 		},
 		link : function(scope, elements, attrs){
-			scope.obj = {
-				'integral_price' : 0
-			}
+			scope.salefrobj = {};
+
 
 			console.log('积分code');
 			console.log(scope.saleobj.code);
-			$resource('/api/us/tc/saleintegral/findsaleintegrallist', {}, {})
-			.save({'integral_sale_code' : scope.saleobj.code}, function(res){
+			$resource('/api/as/tc/saleshangkeprice/getinfo', {}, {})
+			.save({'sale_code' : scope.saleobj.code}, function(res){
 
-				console.log('积分信息');
-				console.log(res);
-				scope.obj = res.data[0];
+				console.log('分润信息');
+				
+				scope.salefrobj = res.data;
+                console.log(res);
 
 
 			});
+
+            scope.saleFrSetSave = function(){
+				alert('添加分润');
+                scope.salefrobj.sale_code = scope.saleobj.code;
+                if(parseInt(scope.salefrobj.profit_ratio) >= 0 && parseInt(scope.salefrobj.profit_ratio) <= 100 && parseInt(scope.salefrobj.rebate_unlimited) >= 0){
+                    $resource('/api/as/tc/saleshangkeprice/save', {}, {})
+                    .save(scope.salefrobj,function(res){
+                        console.log(scope.salefrobj);
+                        if(res.errcode !== 0)
+                        {
+                            alert(res.errmsg);
+                            return;
+                        }
+                        console.log('添加成功');
+
+                        })
+                }
+				
+                
+				// .save({'integral_sale_code':scope.obj.integral_sale_code,'integral_price':scope.obj.integral_price,'list' : array}, function(res){
+				// 	console.log('bbbbbbb');
+				// 	console.log(res);
+				// 	if(res.errcode !== 0)
+				// 	{
+				// 		alert(res.errmsg);
+				// 		return;
+				// 	}
+				// 	console.log('添加成功');
+
+					
+				// });
+
+			};
+
+            $scope.saleFrSetSave = function(){
+		if(parseInt($scope.salefrobj.profit_ratio) >= 0 && parseInt($scope.salefrobj.profit_ratio) <= 100 && parseInt($scope.salefrobj.rebate_unlimited) >= 0){
+			$scope.salefrobj.sale_code = $scope.saleobj.code;
+			saveSaleFenRun.save($scope.salefrobj, function(res){
+		     	if(res.errcode === 0)
+		     	{
+		     		alert('保存成功');
+					$uibModalInstance.close();
+		     	}
+	     		else
+				{
+					alert(res.errmsg);
+				}
+	
+		    });
+		} else {
+			alert('设置正确的利润率(0-100),红包上限不能为负数');
+		}
+	}
 
 		 	
 
