9a3cc9decab8b789b9444b9a7f268320c518756e
malin <ma_0725@163.com>
2017-03-27 14:14:48 +0800

    info_13

--- app/01ticket/controllers/ticketinfo.js
+++ app/01ticket/controllers/ticketinfo.js
@@ -1,4 +1,4 @@
-module.exports = function($scope, $uibModalInstance, $stateParams, info, para, func){
+module.exports = function($scope, $uibModalInstance, $uibModal, $stateParams, info, para, func){
 
 	// console.log('-------');
 	console.log(info);
