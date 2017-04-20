a94bd13557bbc34027e6d4978362bcd0b6cc5dfa
小裙子 <18842678305@163.com>
2017-04-20 14:29:45 +0800

    交通管理

--- app/26ylb/controllers/addTrafficArtical.js
+++ app/26ylb/controllers/addTrafficArtical.js
@@ -3,6 +3,7 @@ module.exports = function($scope, $http, $q, $state,$stateParams, $resource,File
     var id = $stateParams.id;
     console.log(id);
     $scope.obj = {
+        'tyep' : '2',
         'data' : ""
     };
     $scope.infoobj = {};
@@ -37,7 +38,7 @@ module.exports = function($scope, $http, $q, $state,$stateParams, $resource,File
         'pidList' : 
         $http({
             'method' : 'GET',
-            'url' : '/api/as/ic/category/findPidList'
+            'url' : '/api/ac/ic/categoryService/obtainCategory'
         }),
         // 分类信息
         'articalinfo' : 
@@ -65,10 +66,28 @@ module.exports = function($scope, $http, $q, $state,$stateParams, $resource,File
             console.log('shangjiiod');
             console.log(res.pidList.data.data);
             $scope.pid_list = res.pidList.data.data;
+            var pidarray = $scope.pid_list;
+            for(var i = 0; i < pidarray.length; i++){
+                console.log(pidarray[i]);
+                    pidarray[i].title = $scope.getgang(pidarray[i].pathNum)+pidarray[i].title;
+                    // console.log(pidarray[i].title);
+                    console.log($scope.getgang(pidarray[i].pathNum)+pidarray[i].title);
+                
+            }
         } else {
             return;
         }
     });
+    $scope.getgang = function(num){
+        console.log(num);
+        var gang = '';
+        for(var i =0; i < num; i++){
+            gang = gang+'-';
+            
+        }
+        return gang;
+    }
+
      
 
 	$scope.save = function(){
@@ -92,8 +111,75 @@ module.exports = function($scope, $http, $q, $state,$stateParams, $resource,File
     }
 	//取消
 	$scope.cancel = function () {
-		$state.go('app.articallist');
+		$state.go('app.trafficarticallist');
 	};
+
+    // // 百度地图API功能
+	// var map = new BMap.Map("allmap");    // 创建Map实例
+	// map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+	// map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+	// map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+	// map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+	
+	
+	// map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+	// function showInfo(e){
+	// 	alert(e.point.lng + ", " + e.point.lat);
+	// }
+	// map.addEventListener("click", showInfo);
+	
+	
+	// // 百度地图API功能
+	// function G(id) {
+	// 	return document.getElementById(id);
+	// }
+
+	
+
+	// var ac = new BMap.Autocomplete(    //建立一个自动完成的对象
+	// 	{"input" : "suggestId"
+	// 	,"location" : map
+	// });
+
+	// ac.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+	// var str = "";
+	// 	var _value = e.fromitem.value;
+	// 	var value = "";
+	// 	if (e.fromitem.index > -1) {
+	// 		value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+	// 	}    
+	// 	str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+		
+	// 	value = "";
+	// 	if (e.toitem.index > -1) {
+	// 		_value = e.toitem.value;
+	// 		value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+	// 	}    
+	// 	str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+	// 	G("searchResultPanel").innerHTML = str;
+	// });
+
+	// var myValue;
+	// ac.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+	// var _value = e.item.value;
+	// 	myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+	// 	G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+		
+	// 	setPlace();
+	// });
+
+	// function setPlace(){
+	// 	map.clearOverlays();    //清除地图上所有覆盖物
+	// 	function myFun(){
+	// 		var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+	// 		map.centerAndZoom(pp, 18);
+	// 		map.addOverlay(new BMap.Marker(pp));    //添加标注
+	// 	}
+	// 	var local = new BMap.LocalSearch(map, { //智能搜索
+	// 	  onSearchComplete: myFun
+	// 	});
+	// 	local.search(myValue);
+	// }
 
 
  };
