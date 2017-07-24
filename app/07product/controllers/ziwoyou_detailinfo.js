5708fe136b2eb03766113ae445bfd96b5d01912f
马林 <ma_0725@163.com>
2017-07-24 10:21:13 +0800

    1

--- app/07product/controllers/ziwoyou_detailinfo.js
+++ app/07product/controllers/ziwoyou_detailinfo.js
@@ -1,5 +1,5 @@
 module.exports = function ($scope, productNo, $uibModalInstance, $resource) {
-    $resource("/api/uc/dc/ziwoyouService/getProductInfoByCode", {}, {}).save({ productNo: productNo }, function (res) {
+    $resource("/api/uc/dc/ziwoyouService/getProductInfoByCode", {}, {}).save({ productNo: productNo, sale_belong: 'supply_ziwoyou' }, function (res) {
         if (res.errcode === 0) {
             $scope.obj = res.data.product;
         }
